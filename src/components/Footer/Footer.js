8b1ebb7a815d6ce95a2462ed511f8721861799b7
Stiven <stivencastillo.90@gmail.com>
2018-10-25 22:44:08 -0500

    Creación de componentes Header y Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Dumb component / Componente de presentación
+// No tiene estado
+const Footer = () => {
+  return (
+    <footer>
+      <p>Devhack - Workshop Reactjs 2018</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+// Dumb component / Componente de presentación
+// No tiene estado
+const Header = () => {
+  return (
+    <header>
+      <h1>Eventos</h1>
+    </header>
+  );
+};
+
+export default Header;
